2c86f7cc455586f4e2cc7bbe949243de2625f1a8
Mx244xt <dev.se.masuda@gmail.com>
2023-10-24 08:46:47 +0900

    fix: 並べ替え機能の反応位置拡張のためdiv追加

--- src/components/todoForm/Todo/TodoInputFrom.tsx
+++ src/components/todoForm/Todo/TodoInputFrom.tsx
@@ -14,32 +14,36 @@ const TodoInputFrom = ({ isEditing, editedTaskText, isCompleted, inputRef, liste
   const { ref, onChange, ...regist } = register('todo');
 
   return (
-    <div className='flex items-center pr-2  w-80' >
+    <>
       {isEditing ? (
-        <input
-          {...regist}
-          ref={(e) => {
-            inputRef.current = e;
-            ref(e);
-          }}
-          type='text'
-          className='mr-2 py-1 px-2 w-full rounded border-gray-400 border'
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setEditedTaskText(e.target.value);
-            onChange(e);
-          }}
-          value={editedTaskText}
-          placeholder='変更するタスクを入力してください。'
-        />
+        <div className='flex items-center pr-2  w-80' >
+          <input
+            {...regist}
+            ref={(e) => {
+              inputRef.current = e;
+              ref(e);
+            }}
+            type='text'
+            className='mr-2 py-1 px-2 w-full rounded border-gray-400 border'
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setEditedTaskText(e.target.value);
+              onChange(e);
+            }}
+            value={editedTaskText}
+            placeholder='タスクを入力する。'
+          />
+        </div>
       ) : (
-        isCompleted ? (
-          <span className='text-gray-400 line-through flex items-center break-all cursor-pointer' {...listeners}>{editedTaskText}</span>
-        ) : (
-          <span className='text-black flex items-center break-all cursor-pointer' {...listeners}>{editedTaskText}</span>
-        )
+        <div className='flex items-center pr-2 h-14 w-80 cursor-pointer' {...listeners} >
+          {isCompleted ? (
+            <span className='text-gray-400 line-through flex items-center break-all' >{editedTaskText}</span>
+          ) : (
+            <span className='text-black flex items-center break-all' >{editedTaskText}</span>
+          )}
+        </div>
       )
       }
-    </div>
+    </>
   );
 };
 
