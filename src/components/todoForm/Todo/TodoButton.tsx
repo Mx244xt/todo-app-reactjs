096ec31423c0652a01cb7fc95a5b152653690762
Mx244xt <dev.se.masuda@gmail.com>
2023-10-24 08:47:59 +0900

    fix: Todoが完了済みの場合、編集ボタンをdisabledにするよう変更

--- src/components/todoForm/Todo/TodoButton.tsx
+++ src/components/todoForm/Todo/TodoButton.tsx
@@ -2,6 +2,7 @@ import { UseFormHandleSubmit } from "react-hook-form";
 
 interface TodoButtonType {
   isEditing: boolean;
+  isCompleted: boolean;
   handleSubmit: UseFormHandleSubmit<{ todo: string; }, undefined>;
   handleSave: () => Promise<void>;
   handleEdit: () => void;
@@ -9,13 +10,13 @@ interface TodoButtonType {
   handleDelete: () => Promise<void>;
 }
 
-const TodoButton = ({ isEditing, handleSubmit, handleSave, handleEdit, handleReset, handleDelete }: TodoButtonType) => {
+const TodoButton = ({ isEditing, isCompleted, handleSubmit, handleSave, handleEdit, handleReset, handleDelete }: TodoButtonType) => {
   return (
     <div className='flex flex-col items-end w-24 sm:flex-row sm:items-center'>
       {isEditing ? (
         <button type="button" className='text-green-500 mr-3' onClick={handleSubmit(handleSave)}>保存</button>
       ) : (
-        <button type='button' className='text-green-500 mr-3' onClick={handleEdit}>編集</button>
+        <button type='button' className={isCompleted ? 'text-gray-400 mr-3' : 'text-green-500 mr-3'} onClick={handleEdit} disabled={isCompleted} >編集</button>
       )}
       {isEditing ? (
         <button type='button' className='text-red-500 mr-3' onClick={handleReset}>戻る</button>
